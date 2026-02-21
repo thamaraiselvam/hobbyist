0e66f919b64615f36834abf1fd03fc223d71d066
Thamaraiselvam <thamaraiselvam@live.com>
2026-02-21 12:35:52 +0530

    fix: apply dart formatting to daily_tasks_screen_test.dart

--- test/widget/daily_tasks_screen_test.dart
+++ test/widget/daily_tasks_screen_test.dart
@@ -109,7 +109,8 @@ void main() async {
       expect(find.byType(Scaffold), findsOneWidget);
     });
 
-    testWidgets('should display 7 days in day selector', (WidgetTester tester) async {
+    testWidgets('should display 7 days in day selector',
+        (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
           home: DailyTasksScreen(),
@@ -118,8 +119,9 @@ void main() async {
 
       await tester.pumpAndSettle(const Duration(milliseconds: 100));
 
-      final listViewFinder = find.byWidgetPredicate((widget) =>
-        widget is ListView && widget.scrollDirection == Axis.horizontal
+      final listViewFinder = find.byWidgetPredicate(
+        (widget) =>
+            widget is ListView && widget.scrollDirection == Axis.horizontal,
       );
 
       expect(listViewFinder, findsOneWidget);
